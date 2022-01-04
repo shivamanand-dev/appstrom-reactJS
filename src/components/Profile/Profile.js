1b7289af74830ee3361ce65215f14876b9705941
Shivam <shivamanand252@gmail.com>
2022-01-05 01:28:04 +0530

    redux

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+// import { bindActionCreators } from "redux";
+import { fetchUsers } from "../../redux";
 
-const Profile = () => {
-  return <div>Profile</div>;
+const Profile = ({ fetchUsers, userData }) => {
+  // const dispatch = useDispatch();
+  // const state = useSelector((state) => state.user);
+
+  // const { fetchUsers } = bindActionCreators(actionCreators, dispatch);
+
+  useEffect(() => {
+    fetchUsers();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return userData.loading ? (
+    <h2>Loading</h2>
+  ) : userData.error ? (
+    <h2>{userData.error}</h2>
+  ) : (
+    <div>
+      <h2>Users List</h2>
+      <div>
+        {userData &&
+          userData.users &&
+          userData.users.map((user) => <p>{user.name}</p>)}
+      </div>
+    </div>
+  );
 };
 
-export default Profile;
+const mapStateToProps = (state) => {
+  return {
+    userData: state.user,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchUsers: () => dispatch(fetchUsers()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Profile);
---
+++ src/redux/rootreducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import userReducer from "./Users/userReducer";
+
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+export default rootReducer;
