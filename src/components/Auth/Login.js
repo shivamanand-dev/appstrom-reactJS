a3cb4f4c9b20da99f2787059083c73fd91353972
eyekavya <kavyasrivastava7830@gmail.com>
2022-01-17 23:17:47 +0530

    login form designed

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -61,62 +61,62 @@ const Login = ({ setAlert, setNavProgress }) => {
       style={{ display: "flex", justifyContent: "center" }}
     >
       <div className="d-flex justify-content-between" style={{ width: "70%" }}>
-        <div className="text-center" style={{width: "50%"}}>
+        <div className="text-center" style={{ width: "50%" }}>
           <i class="bi bi-person-circle" style={{ fontSize: "200px" }}></i>
         </div>
-        <div style={{width: "50%"}}>
-        <Form style={{ maxWidth: "300px" }} onSubmit={handleSubmit}>
-          <Form.Group className="mb-3">
-            <Form.Label style={{ fontWeight: "500" }} htmlFor="username">
-              Email or Username
-            </Form.Label>
-            <Form.Control
-              type="text"
-              minLength={5}
-              // placeholder="Enter Username or Email"
-              required
-              onChange={handleOnchange}
-              name="username"
-              value={credential.username}
-            />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label style={{ fontWeight: "500" }} htmlFor="password">
-              Password
-            </Form.Label>
-            <Form.Control
-              type="password"
-              minLength={7}
-              // placeholder="Enter Password"
-              required
-              onChange={handleOnchange}
-              name="password"
-              value={credential.password}
-            />
-          </Form.Group>
+        <div style={{ width: "50%" }}>
+          <Form style={{ maxWidth: "300px" }} onSubmit={handleSubmit}>
+            <Form.Group className="mb-3">
+              <Form.Label style={{ fontWeight: "500" }} htmlFor="username">
+                Email or Username
+              </Form.Label>
+              <Form.Control
+                type="text"
+                minLength={5}
+                // placeholder="Enter Username or Email"
+                required
+                onChange={handleOnchange}
+                name="username"
+                value={credential.username}
+              />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label style={{ fontWeight: "500" }} htmlFor="password">
+                Password
+              </Form.Label>
+              <Form.Control
+                type="password"
+                minLength={7}
+                // placeholder="Enter Password"
+                required
+                onChange={handleOnchange}
+                name="password"
+                value={credential.password}
+              />
+            </Form.Group>
 
-          <Button
-            className="authFormBtnTrans"
-            variant="primary"
-            style={{ boxShadow: "none", width: "100%" }}
-            type="submit"
-          >
-            <span>LOG IN</span>
-          </Button>
-
-          <Form.Group style={{ margin: "20px 40px" }} className="mb-3">
-            Not yet Registered? <span> </span>
-            <Link
-              to="/signup"
-              style={{ boxShadow: "none", textDecoration: "none" }}
+            <Button
+              className="authFormBtnTrans"
+              variant="primary"
+              style={{ boxShadow: "none", width: "100%" }}
+              type="submit"
             >
-              Sign up
-            </Link>
-          </Form.Group>
-        </Form>
+              <span>LOG IN</span>
+            </Button>
+
+            <Form.Group style={{ margin: "20px 40px" }} className="mb-3">
+              Not yet Registered? <span> </span>
+              <Link
+                to="/signup"
+                style={{ boxShadow: "none", textDecoration: "none" }}
+              >
+                Sign up
+              </Link>
+            </Form.Group>
+          </Form>
         </div>
       </div>
     </div>
