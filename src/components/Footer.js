4f24d4ec915f6393413cba9d3f89b85ea84c442e
Shivam <shivamanand252@gmail.com>
2022-01-18 03:42:05 +0530

    change footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,34 +9,36 @@ const Footer = () => {
   ) : (
     <>
       <div>
-        <footer className="d-flex bg-light flex-wrap justify-content-between align-items-center py-3 border-top">
-          <div className="container col-md-4 d-flex align-items-center">
-            <Link
-              to="/"
-              className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"
-            >
-              Logo
-            </Link>
-            <span className="text-muted">© 2021 AppStrom</span>
-          </div>
+        <footer className="bg-dark py-3 text-light">
+          <div className="container d-flex flex-wrap justify-content-between align-items-center py-3 border-top">
+            <div className="col-md-4 d-flex align-items-center">
+              <Link
+                to="/"
+                className="mb-3 me-2 mb-md-0 text-light text-decoration-none lh-1"
+              >
+                Logo
+              </Link>
+              <span>© 2021 AppStrom</span>
+            </div>
 
-          <ul className="container nav col-md-4 justify-content-end list-unstyled d-flex">
-            <li className="ms-3 mx-3">
-              <Link className="text-muted" to="#">
-                <i style={{ color: "#1DA1F2" }} className="bi bi-twitter"></i>
-              </Link>
-            </li>
-            <li className="ms-3 mx-3">
-              <Link className="text-muted" to="#">
-                <i style={instaGradient} className="bi bi-instagram"></i>
-              </Link>
-            </li>
-            <li className="ms-3">
-              <Link className="text-muted" to="#">
-                <i style={{ color: "#006de9" }} className="bi bi-meta"></i>
-              </Link>
-            </li>
-          </ul>
+            <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+              <li className="ms-3 mx-3">
+                <Link className="text-muted" to="#">
+                  <i style={{ color: "#1DA1F2" }} className="bi bi-twitter"></i>
+                </Link>
+              </li>
+              <li className="ms-3 mx-3">
+                <Link className="text-muted" to="#">
+                  <i style={instaGradient} className="bi bi-instagram"></i>
+                </Link>
+              </li>
+              <li className="ms-3">
+                <Link className="text-muted" to="#">
+                  <i style={{ color: "#006de9" }} className="bi bi-meta"></i>
+                </Link>
+              </li>
+            </ul>
+          </div>
         </footer>
       </div>
     </>
