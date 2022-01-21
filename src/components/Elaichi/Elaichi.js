991a7d9a0010cc19c07ecbe3cd7b69237a8998ff
shivamanand-dev <shivam_anand@outlook.com>
2022-01-22 02:28:29 +0530

    added elaichi post and card button

--- src/components/Elaichi/Elaichi.js
+++ src/components/Elaichi/Elaichi.js
@@ -1,7 +1,39 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Card, Button, Form } from "react-bootstrap";
 
 const Elaichi = () => {
-  return <div>Elaichi</div>;
+  return (
+    <div style={{ maxWidth: "500px", margin: "0 auto" }}>
+      {/* Elaichi wala Form */}
+      <Form className="mb-3">
+        <Form.Group controlId="exampleForm.ControlTextarea1">
+          <Form.Control
+            placeholder="What's Happening?? 😉"
+            as="textarea"
+            rows={3}
+          />
+        </Form.Group>
+        <Button
+          variant="primary"
+          style={{ boxShadow: "none", width: "100%" }}
+          type="submit"
+        >
+          Post
+        </Button>
+      </Form>
+      <Card>
+        <Card.Body>
+          <Card.Title>Special title treatment</Card.Title>
+          <Card.Text>
+            With supporting text below as a natural lead-in to additional
+            content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 };
 
 export default Elaichi;
--- src/redux/Elaichi/elaichiAction.js
+++ src/redux/Elaichi/elaichiAction.js
@@ -1,8 +1,26 @@
+import axios from "axios";
 import {
   FETCH_ELAICHI_REQUEST,
   FETCH_ELAICHI_SUCCESS,
   FETCH_ELAICHI_FAILURE,
 } from "./elaichiTypes";
+
+const REACT_APP_ELAICHI_BASE_URL = process.env.REACT_APP_ELAICHI_BASE_URL;
+
+export const getAllElaichi = () => {
+  return (dispatch) => {
+    dispatch(fetchElaichiRequest());
+    axios
+      .get(`${REACT_APP_ELAICHI_BASE_URL}/`)
+      .then((res) => {
+        const elaichi = res.data;
+        dispatch(fetchElaichiSuccess(elaichi));
+      })
+      .catch((error) => {
+        dispatch(fetchElaichiFailure(error.message));
+      });
+  };
+};
 
 export const fetchElaichiRequest = () => {
   return {
