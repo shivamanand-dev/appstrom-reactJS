53ffdce659bc7682bdba886dc3b49139a4ab40ba
shivamanand-dev <shivam_anand@outlook.com>
2022-01-22 01:22:10 +0530

    added root import in reducers

--- src/redux/index.js
+++ src/redux/index.js
@@ -2,3 +2,4 @@ export * from "./Users/userAction";
 export * from "./Progress/progressAction";
 export * from "./Alert/alertAction";
 export * from "./ActivityTracker/activityAction";
+export * from "./Elaichi/elaichiAction";
--- src/redux/rootreducer.js
+++ src/redux/rootreducer.js
@@ -3,12 +3,14 @@ import userReducer from "./Users/userReducer";
 import progressReducer from "./Progress/progressReducer";
 import alertReducer from "./Alert/alertReducer";
 import activityReducer from "./ActivityTracker/activityReducer";
+import elaichi from "./Elaichi/elaichiReducer";
 
 const rootReducer = combineReducers({
   user: userReducer,
   progress: progressReducer,
   alert: alertReducer,
   activity: activityReducer,
+  elaichi: elaichi,
 });
 
 export default rootReducer;
