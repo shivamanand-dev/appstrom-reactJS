f15e6c0c852161cbc7f37f43906b4f94a4eb58f2
shivamanand-dev <shivam_anand@outlook.com>
2022-01-24 23:21:15 +0530

    remove footer bug

--- src/components/GlobalUi/Footer.js
+++ src/components/GlobalUi/Footer.js
@@ -8,6 +8,13 @@ const Footer = () => {
     <></>
   ) : (
     <>
+      <div
+        style={{
+          height: "130px",
+          bottom: "0",
+          width: "100%",
+        }}
+      ></div>
       <div>
         <footer className="bg-dark py-3 text-light">
           <div className="container d-flex flex-wrap justify-content-between align-items-center py-3 border-top">
@@ -26,7 +33,10 @@ const Footer = () => {
               className="nav col-md-4 justify-content-end list-unstyled d-flex"
             >
               <li className="ms-3 mx-3">
-                <a className="text-muted" href="https://twitter.com/app_strom">
+                <a
+                  className="text-muted"
+                  href="https://twitter.com/appstrom_app"
+                >
                   <i style={{ color: "#1DA1F2" }} className="bi bi-twitter"></i>
                 </a>
               </li>
