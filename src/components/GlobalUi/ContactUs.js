361071b5f809679848f2d363143f99adedaea754
eyekavya <kavyasrivastava7830@gmail.com>
2022-02-03 22:57:02 +0530

    contact page ui completed

--- src/components/GlobalUi/ContactUs.js
+++ src/components/GlobalUi/ContactUs.js
@@ -4,30 +4,45 @@ import { Form, Button } from "react-bootstrap";
 
 const ContactUs = () => {
   return (
-    <div style={{ maxWidth: "500px", margin: "0 auto" }}>
-      <Form>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label>Name</Form.Label>
-          <Form.Control type="text" placeholder="Name" />
-        </Form.Group>
+    <div className="gradientBackground">
+      <div
+        className="authContainer"
+        style={{
+          width: "100%",
+          maxWidth: "500px",
+          margin: "0 auto",
+          padding: "60px",
+        }}
+      >
+        <Form>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label style={{ fontWeight: "500" }}>Name</Form.Label>
+            <Form.Control type="text" placeholder="Name" />
+          </Form.Group>
 
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="name@example.com" />
-        </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label style={{ fontWeight: "500" }}>Email address</Form.Label>
+            <Form.Control type="email" placeholder="name@example.com" />
+          </Form.Group>
 
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Message</Form.Label>
-          <Form.Control
-            as="textarea"
-            rows={4}
-            placeholder="Type your message here"
-          />
-        </Form.Group>
-        <Button variant="primary" type="submit" style={{ width: "100%" }}>
-          Submit
-        </Button>
-      </Form>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label style={{ fontWeight: "500" }}>Message</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={4}
+              placeholder="Type your message here"
+            />
+          </Form.Group>
+          <Button
+            className="authFormBtnTrans"
+            variant="primary"
+            type="submit"
+            style={{ width: "100%" }}
+          >
+            Submit
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 };
