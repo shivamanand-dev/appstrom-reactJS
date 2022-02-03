708ed181465a980bad934e52414c9e0730da45f9
shivamanand-dev <shivam_anand@outlook.com>
2022-02-03 21:47:02 +0530

    bug fix

--- src/components/GlobalUi/ContactUs.js
+++ src/components/GlobalUi/ContactUs.js
@@ -7,9 +7,15 @@ const ContactUs = () => {
     <div style={{ maxWidth: "500px", margin: "0 auto" }}>
       <Form>
         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="text" placeholder="Name" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="name@example.com" />
         </Form.Group>
+
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
           <Form.Label>Message</Form.Label>
           <Form.Control
