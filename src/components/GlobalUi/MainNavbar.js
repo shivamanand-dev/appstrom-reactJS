d72936150fd6d14191725e48a3b264176c525282
shivamanand-dev <shivam_anand@outlook.com>
2022-02-07 22:20:24 +0530

    bug removed

--- src/components/GlobalUi/MainNavbar.js
+++ src/components/GlobalUi/MainNavbar.js
@@ -36,7 +36,7 @@ const MainNavbar = () => {
         <Container className="container">
           {/* <Navbar.Brand style={{ fontSize: "25px", fontWeight: "500" }}> */}
           <Link to="/" style={{ textDecoration: "none" }}>
-            <Navbar.Brand style={{ fontSize: "25px", fontWeight: "500" }}>
+            <Navbar.Brand>
               <img src={logo} width="100px" alt="appstrom" />
             </Navbar.Brand>
           </Link>
